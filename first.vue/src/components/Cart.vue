801b744141a9d0a5a6f25eb6896262a1dadda112
student <student@GFL-1466.gfl.geeksforless.net>
2019-05-03 18:51:58 +0300

    Files

---
+++ first.vue/src/components/Cart.vue
@@ -0,0 +1,47 @@
+<template>
+    <div id="app">
+        <h1>Cart!</h1>
+        <div class="cart">
+      <h3>Cart:</h3>
+      <ul>
+        <li v-for="(item, key) in ingrArr" v-if="item.count" :key="key" :item="item" style="margin-bottom: 5px">
+          <button type="button" @click="remove(item)">-</button>
+          <span class="cart__name">{{ item.name }} x {{ item.count }}</span> - 
+          <span class="cart__price">{{ item.price*item.count }}</span>
+        </li>
+      </ul>
+      <span class="cart__total">Total: {{ total__price }}</span>
+    </div>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+       return{
+ingrArr: JSON.parse(localStorage.ingr || '')
+       }
+   } ,
+
+   
+   methods: {    
+    remove: function(item) {
+      item.count--;
+      localStorage.ingr = JSON.stringify(this.ingrArr);
+    }
+  },
+  computed: {
+    total__price: function() {
+      let total = 0;
+      this.ingrArr.forEach((el) => {
+        if (el.count) {
+          total += el.price * el.count;
+        }
+      });
+      return total;
+    },
+  },
+}
+</script>
+<style >
+    
+</style>
---
+++ first.vue/src/components/Shop.vue
@@ -0,0 +1,80 @@
+<template>
+  <div id="app">
+    <h1>Shop!</h1>
+    <div class="catalog">
+      <h3>Catalog:</h3>
+      <ul>
+        <li v-for="(item, key) in ingrArr" :key="key" :item="item" style="margin-bottom: 5px">
+          <button type="button" @click="add(item)">+</button>
+          <span class="catalog__name">{{ item.name }}</span> -
+          <span class="catalog__price">{{ item.price }}</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+//import Products from '@/shop/products'
+var ingredients = [
+  {
+    id: 1,
+    name: "milk",
+    price: 100,
+    count: 0
+  },
+  {
+    id: 2,
+    name: "bread",
+    price: 200,
+    count: 0
+  },
+  {
+    id: 3,
+    name: "oil",
+    price: 300,
+    count: 0
+  },
+  {
+    id: 4,
+    name: "apple",
+    price: 400,
+    count: 0
+  },
+  {
+    id: 5,
+    name: "carrot",
+    price: 500,
+    count: 0
+  },
+  {
+    id: 6,
+    name: "banana",
+    price: 600,
+    count: 0
+  },
+  {
+    id: 65,
+    name: "teeeeeest",
+    price: 600,
+    count: 0
+  }
+];
+export default {
+  data() {
+    return {
+      ingrArr: JSON.parse(localStorage.ingr || JSON.stringify(ingredients))
+    };
+  },
+  methods: {
+    add: function(item) {
+      item.count++;
+      localStorage.ingr = JSON.stringify(this.ingrArr);
+    }
+  },
+  components: {
+    // Products
+  }
+};
+</script>
+<style >
+</style>
